858304cf543b748cad1d7385607a0f0cfe2eb513
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-18 09:08:45 -0300

    chore: componentizing the attributes

--- lista_pelada/src/components/Detalhes/index.tsx
+++ lista_pelada/src/components/Detalhes/index.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import { Acoes, Botao, Card, Descricao, Tag, Titulo } from "./styles";
 
-export const Detalhes = () => {
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+export const Detalhes = ({ descricao, prioridade, status, titulo }: Props) => {
   const [estaEditando, setEstaEditando] = useState(false)
 
   return (
     <Card>
-      <Titulo>detalhes do peladeiro</Titulo>
-      <Tag>mensalista</Tag>
-      <Tag>diarista</Tag>
-      <Tag>amarelo</Tag>
-      <Tag>azul</Tag>
-      <Tag>assiduo</Tag>
-      <Descricao>Essse atleta é assiduo</Descricao>
+      <Titulo>{titulo}</Titulo>
+      <Tag>{prioridade}</Tag>
+      <Tag>{status}</Tag>
+      <Descricao value={descricao} />
       <Acoes>
         {estaEditando ? (
           <>
--- lista_pelada/src/containers/Lista/index.tsx
+++ lista_pelada/src/containers/Lista/index.tsx
@@ -1,22 +1,37 @@
 import { Detalhes } from "../../components/Detalhes";
 import { Container } from "./styles";
 
+const situacao = [
+  {
+    titulo: "Chagou 1",
+    prioridade: 'mensalista',
+    status: 'Time Amarelo',
+    descricao: 'tudo certo'
+  },
+  {
+    titulo: "Chagou 2",
+    prioridade: 'diarista',
+    status: 'Time Laranja',
+    descricao: 'Faltou sabado passado'
+  },
+  {
+    titulo: "Chagou 3",
+    prioridade: 'mensalista',
+    status: 'Time Azul',
+    descricao: 'Tudo certo'
+  },
+]
+
 export const Lista = () => (
   <Container>
     <p>Total de peladeiros: 2 Mensalistas e 5 Diaristas</p>
     <ul>
-      <li>
-        <Detalhes />
-      </li>
-      <li>
-        <Detalhes />
-      </li>
-      <li>
-        <Detalhes />
-      </li>
-      <li>
-        <Detalhes />
-      </li>
+      {situacao.map((s) => (
+        <li key={s.titulo}>
+          <Detalhes titulo={s.titulo} prioridade={s.prioridade} status={s.status} descricao={s.descricao} />
+        </li>
+      ))}
+
     </ul>
   </Container>
 )
