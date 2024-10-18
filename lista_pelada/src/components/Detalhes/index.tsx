a05060f18b69cca5cfddd1301f2b6873a9c7f205
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-18 08:31:12 -0300

    chore: inserted actions in the buttons

--- lista_pelada/src/components/Detalhes/index.tsx
+++ lista_pelada/src/components/Detalhes/index.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Acoes, Botao, Card, Descricao, Tag, Titulo } from "./styles";
 
-export const Detalhes = () => (
-  <Card>
-    <Titulo>detalhes do peladeiro</Titulo>
-    <Tag>mensalista</Tag>
-    <Tag>diarista</Tag>
-    <Tag>amarelo</Tag>
-    <Tag>azul</Tag>
-    <Tag>assiduo</Tag>
-    <Descricao>Essse atleta é assiduo</Descricao>
-    <Acoes>
-      <Botao>Editar</Botao>
-      <Botao>Remover</Botao>
-    </Acoes>
-  </Card>
-)
+export const Detalhes = () => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <Card>
+      <Titulo>detalhes do peladeiro</Titulo>
+      <Tag>mensalista</Tag>
+      <Tag>diarista</Tag>
+      <Tag>amarelo</Tag>
+      <Tag>azul</Tag>
+      <Tag>assiduo</Tag>
+      <Descricao>Essse atleta é assiduo</Descricao>
+      <Acoes>
+        {estaEditando ? (
+          <>
+            <Botao>Salvar</Botao>
+            <Botao onClick={() => setEstaEditando(false)}>Cancelar</Botao>
+          </>
+        ) : (
+          <>
+            <Botao onClick={() => setEstaEditando(true)}>Editar</Botao>
+            <Botao>Remover</Botao>
+          </>
+        )}
+      </Acoes>
+    </Card>
+  )
+}
