27e2fcf14dcb83160b8b75f58bcc3adf23fc15d1
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-18 17:30:25 -0300

    chore: website responsiveness

--- lista_pelada/src/containers/BarraLateral/styles.ts
+++ lista_pelada/src/containers/BarraLateral/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { breakpoints } from '../../styles';
 
 export const Logo = styled.div`
   display: flex;
@@ -10,12 +11,22 @@ export const Logo = styled.div`
     width: 250px;
     border-radius: 80px;
   }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    img {
+      width: 150px;
+    }
+  }
 `;
 
 export const Aside = styled.aside`
   background-color: #eee;
   height: 100vh;
   padding: 26px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    height: 80vh;
+  }
 `;
 
 export const Filtros = styled.div`
--- lista_pelada/src/styles/index.ts
+++ lista_pelada/src/styles/index.ts
@@ -1,5 +1,9 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
+export const breakpoints = {
+  tablet: '40rem',
+};
+
 export const EstiloGlobal = createGlobalStyle`
 * {
   margin: 0;
@@ -13,4 +17,8 @@ export const EstiloGlobal = createGlobalStyle`
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 320px auto;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    grid-template-columns: 1fr;
+  }
 `;
